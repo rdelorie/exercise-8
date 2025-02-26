5696b3ba09097c8f09a99edac6b8544f90ced12d
Rachel <rdelorie@colostate.edu>
2025-02-26 10:12:54 -0700

    commit r script

--- day-08.R
+++ day-08.R
@@ -6,6 +6,16 @@ covid_data <- read_csv("https://raw.githubusercontent.com/nytimes/covid-19-data/
 # step 2
 df = data.frame(region = state.region, 
 abbreviation = state.abb, state = state.name) %>% 
-  left_join(covid_data, df, by = c("state" = "state")) #step 3
+  inner_join(covid_data, df, by = "state") %>%  #step 3
+  group_by(region, date) 
 
-# step 4
+df %>% 
+summarize(cases = sum(cases),
+          deaths = sum(deaths)) %>% # step 4
+  pivot_longer(cols = c(cases, deaths), # step 5
+               names_to = "type",
+               values_to = "count") %>% 
+  ggplot(aes(x = date, y = count)) + # step 6
+  geom_line() +
+  facet_grid(type~region, scales = "free_y")+
+  theme_bw()
